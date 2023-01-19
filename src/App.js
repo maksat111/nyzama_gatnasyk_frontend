95868e062a24792f191a54f4cb2fa908e5ccc446
maksat111 <maksatakmyradow111@gmail.com>
2023-01-19 19:34:45 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Welcome from './pages/Welcome/Welcome';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Welcome />
     </div>
   );
 }
