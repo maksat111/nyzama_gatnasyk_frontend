9fa527a998ea09726d35114d25267af2452aa2f3
maksat111 <maksatakmyradow111@gmail.com>
2023-03-06 05:04:31 +0500

    complated

--- src/pages/Hasabat/Hasabat.js
+++ src/pages/Hasabat/Hasabat.js
@@ -1,9 +1,14 @@
 import { React, useState, useEffect } from 'react';
-import { Input, Table, message, Space } from 'antd';
+import { Input, Table, message, Space, DatePicker } from 'antd';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import date from 'date-and-time';
 import { axiosInstance } from '../../config/axios';
 import '../Gatnasyk/Gatnasyk.css';
 import './Hasabat.css';
 import Checkbox from 'antd/es/checkbox/Checkbox';
+dayjs.extend(customParseFormat);
+
 
 function Gatnasyk() {
     const [gozlegInput, setGozlegInput] = useState(null);
@@ -11,7 +16,12 @@ function Gatnasyk() {
     const [groups, setGroups] = useState([]);
     const [activeSidebar, setActiveSidebar] = useState(null);
     const [tableData, setTableData] = useState([]);
+    const today = date.format(new Date(), 'YYYY-MM-DD');
+    const [selectedDate, setSelectedDate] = useState(today);
+    const dateFormat = 'YYYY-MM-DD';
 
+
+    // Table datas and settings
     const columns = [
         {
             title: 'Ady',
@@ -28,16 +38,8 @@ function Gatnasyk() {
             key: 'late',
             dataIndex: 'late',
             width: '150px',
-            onChange: (record) => console.log(record),
             render: (_, record) => (
-                <Checkbox name='late' onChange={(e) => {
-                    const index = tableData.findIndex(item => item.key == record.key);
-                    setTableData(previousState => {
-                        let a = [...previousState];
-                        a[index].late = e.target.checked;
-                        return a;
-                    })
-                }} />
+                <Checkbox name='late' disabled checked={record.late} />
             ),
 
         },
@@ -47,14 +49,7 @@ function Gatnasyk() {
             dataIndex: 'uniform',
             width: '200px',
             render: (_, record) => (
-                <Checkbox style={{ color: 'rgb(7, 7, 175)' }} name='uniform' onChange={(e) => {
-                    const index = tableData.findIndex(item => item.key == record.key);
-                    setTableData(previousState => {
-                        let a = [...previousState];
-                        a[index].uniform = e.target.checked;
-                        return a;
-                    })
-                }} />
+                <Checkbox name='late' disabled checked={record.uniform} />
             ),
         },
         {
@@ -64,16 +59,10 @@ function Gatnasyk() {
             render: (_, record) => (<Input
                 name='note'
                 style={{ width: '320px' }}
-                placeholder='Bellik...'
+                placeholder=' '
                 bordered={false}
-                onChange={(e) => {
-                    const index = tableData.findIndex(item => item.key == record.key);
-                    setTableData(previousState => {
-                        let a = [...previousState];
-                        a[index].note = e.target.value;
-                        return a;
-                    })
-                }}
+                disabled
+                value={record.note}
             />
             ),
             width: '400px'
@@ -85,28 +74,37 @@ function Gatnasyk() {
             let tableData = [];
             setGroups(data.data.data);
             setActiveSidebar(data.data.data[0]);
-            const table = await axiosInstance.get(`students/${data.data.data[0]._id}`);
-            table.data.data.map((item) => tableData.push({
-                key: item._id,
-                name: item.name,
-                surname: item.surname,
-                late: false,
-                uniform: false,
-                note: null
-            }))
+            let table = await axiosInstance.get(`breaking?group_id=${data.data.data[0]._id}&&created_at=${today}`);
+
+            table.data.data.forEach(element => {
+                const index = element.breakingRules.findIndex(item => item.created_at == today);
+
+                tableData.push({
+                    key: element._id,
+                    name: element.name,
+                    surname: element.surname,
+                    late: index == -1 ? false : element.breakingRules[index].late,
+                    uniform: index == -1 ? false : element.breakingRules[index].uniform,
+                    note: index == -1 ? null : element.breakingRules[index].note
+                })
+            });
+
             setTableData(tableData);
         }).catch(err => {
-            message.error('Yalnyslyk!')
+            console.log(err)
+            message.error('Yalnyslyyyyk!')
         })
     }, [])
 
+    // search options and etc
     const handleSearch = (event) => {
         setGozlegInput(event.target.value);
     }
 
     useEffect(() => {
         if (gozlegInput) {
-            axiosInstance.get('/groups/search').then((res) => {
+            axiosInstance.get(`/groups/search?group_number=${gozlegInput}`).then((res) => {
+                console.log(res)
                 setGozlegData(res.data.data);
             }).catch((err) => {
                 message.error('Yalnyslyk!');
@@ -114,72 +112,96 @@ function Gatnasyk() {
         }
     }, [gozlegInput])
 
-    const handleSidebarSelect = async (item) => {
-        let tableData = [];
-        setActiveSidebar(item);
-        const table = await axiosInstance.get(`students/${item._id}`);
-        table.data.data.map((item) => tableData.push({
-            key: item._id,
-            name: item.name,
-            surname: item.surname,
-            late: false,
-            uniform: false,
-            note: null
-        }))
-        setTableData(tableData);
-    }
 
-    const handleConfirm = async () => {
-        const filtered = tableData.filter(item => item.late == true || item.uniform == true || item.note !== null);
-        filtered.forEach(element => {
-            element.student_id = element.key;
-            delete element.key
-            delete element.name
-            delete element.surname
+    ///sidebar functions
+    const handleSidebarSelect = async (item) => {
+        setActiveSidebar(item);
+        let newData = []
+        console.log(selectedDate)
+        let table = await axiosInstance.get(`breaking?group_id=${item._id}&&created_at=${selectedDate}`);
+
+        table.data.data.forEach(element => {
+            const index = element.breakingRules.findIndex(item => item.created_at == selectedDate);
+
+            newData.push({
+                key: element._id,
+                name: element.name,
+                surname: element.surname,
+                late: index == -1 ? false : element.breakingRules[index].late,
+                uniform: index == -1 ? false : element.breakingRules[index].uniform,
+                note: index == -1 ? null : element.breakingRules[index].note
+            })
         });
 
-        const index = groups.findIndex((item) => item._id == activeSidebar._id);
-        const lastIndex = groups.length - 1;
+        setTableData(newData);
+    }
 
-        index == lastIndex ? setActiveSidebar(groups[0]) : setActiveSidebar(groups[index + 1]);
+    const handleDatePicker = async (date, dateString) => {
+        try {
+            setSelectedDate(dateString);
+            let newData = []
 
-        await axiosInstance.post('breaking/create', filtered);
-        message.success('Üstünlikli tassyklandy!');
-        let a = [];
-        const table = await axiosInstance.get(`students/${index == lastIndex ? groups[0]._id : groups[index + 1]._id}`);
-        table.data.data.map((item) => a.push({
-            key: item._id,
-            name: item.name,
-            surname: item.surname,
-            late: false,
-            uniform: false,
-            note: null
-        }))
-        setTableData(a);
+            let table = await axiosInstance.get(`breaking?group_id=${activeSidebar._id}&&created_at=${dateString}`);
+
+            table.data.data.forEach(element => {
+                const index = element.breakingRules.findIndex(item => item.created_at == dateString);
+
+                newData.push({
+                    key: element._id,
+                    name: element.name,
+                    surname: element.surname,
+                    late: index == -1 ? false : element.breakingRules[index].late,
+                    uniform: index == -1 ? false : element.breakingRules[index].uniform,
+                    note: index == -1 ? null : element.breakingRules[index].note
+                })
+            });
+
+            setTableData(newData);
+        } catch (err) {
+            message.error("Yalnyslyk!")
+        }
     }
 
     return (
         <div className='gatnasyk_page' >
             <div className='gatnasyk_sidebar_container'>
                 <Input style={{ marginBottom: '15px' }} size='large' placeholder='Gözleg...' onChange={handleSearch} value={gozlegInput} allowClear />
-                {groups?.map((item) =>
-                    <div
-                        className={item == activeSidebar ? `gatnasyk_sidebar_item active_gatnasyk_sidebar` : 'gatnasyk_sidebar_item'}
-                        key={item._id}
-                        onClick={() => handleSidebarSelect(item)}
-                    >
-                        <p>{item.group_number}</p>
-                    </div>
-                )}
+                {gozlegInput == null || gozlegInput == ''
+                    ? groups?.map((item) =>
+                        <div
+                            className={item == activeSidebar ? `gatnasyk_sidebar_item active_gatnasyk_sidebar` : 'gatnasyk_sidebar_item'}
+                            key={item._id}
+                            onClick={() => handleSidebarSelect(item)}
+                        >
+                            <p>{item.group_number}</p>
+                        </div>
+                    )
+                    : gozlegdata?.map((item) =>
+                        <div
+                            className={item == activeSidebar ? `gatnasyk_sidebar_item active_gatnasyk_sidebar` : 'gatnasyk_sidebar_item'}
+                            key={item._id}
+                            onClick={() => handleSidebarSelect(item)}
+                        >
+                            <p>{item.group_number}</p>
+                        </div>
+                    )}
             </div>
             <div className='gatnasyk_right_side'>
+                <div className='datePicker_container'>
+                    <DatePicker
+                        placeholder='Wagtyny saýlaň...'
+                        size='large'
+                        style={{ width: '200px' }}
+                        defaultValue={dayjs(today, dateFormat)}
+                        onChange={handleDatePicker}
+                    />
+                </div>
                 <Table
                     style={{ fontSize: '17px' }}
                     columns={columns}
                     dataSource={tableData}
                     pagination={false}
                 />
-                <div className='tassykla_button' onClick={handleConfirm}>Tassyklamak</div>
             </div>
         </div >
     );
